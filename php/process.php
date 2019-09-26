e11f7ad664bb7bb301c21f373ab31016c0274be6
joelandrews4 <45742059+joelandrews4@users.noreply.github.com>
2019-09-26 21:54:30 +0100

    contact form added

--- php/process.php
+++ php/process.php
@@ -1,32 +1,10 @@
-<?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["message"];
- 
-$EmailTo = "joelandrews4@gmail.com";
-$Subject = "New Message Received";
- 
-// prepare email body text
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
- 
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
- 
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
- 
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
- 
-// redirect to success page
-if ($success){
-   echo "success";
-}else{
-    echo "invalid";
-}
- 
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "joelandrews4@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
