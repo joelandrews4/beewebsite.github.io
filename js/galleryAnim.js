f229a90b8711c706a3c7065130142d22ab5d5bca
joelandrews4 <45742059+joelandrews4@users.noreply.github.com>
2019-10-07 11:11:40 +0100

    fixed lag?

--- js/galleryAnim.js
+++ js/galleryAnim.js
@@ -1,9 +1,13 @@
-const gallery = document.querySelector('.gallery');
-const jumbotron = document.querySelector('.jumbotron-anim-container');
-const jumbotronText = document.querySelector('.jumbotron-container');
+document.addEventListener("DOMContentLoaded", function (event) {
+    window.onload = function () {
+        const gallery = document.querySelector('.gallery');
+        const jumbotron = document.querySelector('.jumbotron-anim-container');
+        const jumbotronText = document.querySelector('.jumbotron-container');
 
-const tl = new TimelineMax();
+        const tl = new TimelineMax();
 
-tl.fromTo(jumbotron,3, {x: '-10%', opacity: 0}, {x: '0%', opacity: 1})
-.fromTo(jumbotronText, 3, {x: '-10%'}, {x: '0%'}, "-=3")
-.fromTo(gallery, 3, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut}, "-=2");
+        tl.fromTo(jumbotron, 3, { x: '-10%', opacity: 0 }, { x: '0%', opacity: 1 })
+            .fromTo(jumbotronText, 3, { x: '-10%' }, { x: '0%' }, "-=3")
+            .fromTo(gallery, 3, { opacity: 0 }, { opacity: 1, ease: Power2.easeInOut }, "-=2");
+    };
+});
--- js/indexAnim.js
+++ js/indexAnim.js
@@ -1,7 +1,12 @@
-const carousel = document.querySelector('.top-carousel');
-const containerFeatures = document.querySelector('.container-features');
+document.addEventListener("DOMContentLoaded", function (event) {
+    window.onload = function () {
+        const carousel = document.querySelector('.top-carousel');
+        const containerFeatures = document.querySelector('.container-features');
 
-const tl = new TimelineMax();
+        const tl = new TimelineMax();
 
-tl.fromTo(carousel, 3, {x: '-10%', opacity: 0}, {x: '0%', opacity: 1, ease: Power2.easeInOut})
-.fromTo(containerFeatures, 3, {opacity: 0}, {opacity: 1, ease: Power2.easeInOut}, "-=2");
+        tl.fromTo(carousel, 3, { x: '-10%', opacity: 0 }, { x: '0%', opacity: 1, ease: Power2.easeInOut })
+            .fromTo(containerFeatures, 3, { opacity: 0 }, { opacity: 1, ease: Power2.easeInOut }, "-=2");
+    };
+});
+
