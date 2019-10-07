9cabb6159b9df7d54798a38fe6a04c0148886c30
joelandrews4 <45742059+joelandrews4@users.noreply.github.com>
2019-10-07 16:08:58 +0100

    fixed animations

--- js/galleryAnim.js
+++ js/galleryAnim.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
         const gallery = document.querySelector('.gallery');
         const jumbotron = document.querySelector('.jumbotron-anim-container');
         const jumbotronText = document.querySelector('.jumbotron-container');
+        const galleryContainer = document.querySelector('.gallery-container');
+        const loadingAnim = document.querySelector('.lds-hive');
 
         const tl = new TimelineMax();
 
-        tl.fromTo(jumbotron, 3, { x: '-10%', opacity: 0 }, { x: '0%', opacity: 1 })
+        tl.fromTo(galleryContainer, 1, { opacity: 0 }, { opacity: 1 })
+            .fromTo(loadingAnim, 1, { opacity: 1 }, { opacity: 0 }, "-=1")
+            .fromTo(jumbotron, 3, { x: '-10%', opacity: 0 }, { x: '0%', opacity: 1 })
             .fromTo(jumbotronText, 3, { x: '-10%' }, { x: '0%' }, "-=3")
-            .fromTo(gallery, 3, { opacity: 0 }, { opacity: 1, ease: Power2.easeInOut }, "-=2");
+            .fromTo(gallery, 3, { opacity: 0 }, { opacity: 1 }, "-=2");
     };
 });
--- js/indexAnim.js
+++ js/indexAnim.js
@@ -2,11 +2,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
     window.onload = function () {
         const carousel = document.querySelector('.top-carousel');
         const containerFeatures = document.querySelector('.container-features');
+        const indexMain = document.querySelector('.index-main');
+        const loadingAnim = document.querySelector('.lds-hive');
 
         const tl = new TimelineMax();
 
-        tl.fromTo(carousel, 3, { x: '-10%', opacity: 0 }, { x: '0%', opacity: 1, ease: Power2.easeInOut })
-            .fromTo(containerFeatures, 3, { opacity: 0 }, { opacity: 1, ease: Power2.easeInOut }, "-=2");
+        tl.fromTo(indexMain, 1, { opacity: 0 }, { opacity: 1 })
+            .fromTo(loadingAnim, 1, { opacity: 1 }, { opacity: 0 }, "-=1")
+            .fromTo(carousel, 3, { x: '-10%', opacity: 0 }, { x: '0%', opacity: 1 })
+            .fromTo(containerFeatures, 3, { opacity: 0 }, { opacity: 1}, "-=2");
     };
 });
 
